88dd5bd33a24888bb443510e5c6e82e91abfa4a4
Wang Di <wangdi@xiaobangtouzi.com>
2019-06-09 20:20:26 +0800

    广告投放系统的实体类创建

---
+++ ad-service/ad-sponsor/src/main/java/com/ad/entity/AdUser.java
@@ -0,0 +1,54 @@
+package com.ad.entity;
+
+import com.ad.constant.CommonStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @Author : wangdi
+ * @Date : create in 2019/6/9 19:55
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ad_user")
+public class AdUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Basic
+    @Column(name = "user_name", nullable = false)
+    private String username;
+
+    @Basic
+    @Column(name = "token", nullable = false)
+    private String token;
+
+    @Basic
+    @Column(name = "user_status", nullable = false)
+    private Integer userStatus;
+
+    @Basic
+    @Column(name = "create_time", nullable = false)
+    private Date createTime;
+
+    @Basic
+    @Column(name = "update_time", nullable = false)
+    private Date updateTime;
+
+    public AdUser(String username, String token){
+        this.username = username;
+        this.token = token;
+        this.userStatus = CommonStatus.VALID.getStatus();
+        this.createTime = new Date();
+        this.updateTime = createTime;
+    }
+}
