1b8203e2909565635bc8e6c6ea038d947726a62b
Wang Di <wangdi@xiaobangtouzi.com>
2019-06-09 11:07:14 +0800

    init zuul application node

---
+++ ad-gateway/src/main/java/com/ad/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author : wangdi
+ * @Date : create in 2019/6/9 09:59
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulGatewayApplication.class);
+    }
+}
---
+++ ad-gateway/src/main/java/com/ad/filter/PreRequestFilter.java
@@ -0,0 +1,39 @@
+package com.ad.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author : wangdi
+ * @Date : create in 2019/6/9 10:56
+ */
+@Slf4j
+@Component
+public class PreRequestFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext context = RequestContext.getCurrentContext();
+        context.set("startTime",System.currentTimeMillis());
+        return null;
+    }
+}
