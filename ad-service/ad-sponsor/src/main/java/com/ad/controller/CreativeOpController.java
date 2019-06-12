3db8561dac70c24f6642e967c6c23c68261d5dba
Wang Di <wangdi@xiaobangtouzi.com>
2019-06-13 00:07:20 +0800

    创意创建接口编写

---
+++ ad-service/ad-sponsor/src/main/java/com/ad/controller/CreativeOpController.java
@@ -0,0 +1,31 @@
+package com.ad.controller;
+
+import com.ad.exception.AdException;
+import com.ad.service.ICreativeService;
+import com.ad.vo.CreativeRequest;
+import com.ad.vo.CreativeResponse;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author : wangdi
+ * @Date : create in 2019/6/12 23:53
+ */
+@Slf4j
+@RestController
+public class CreativeOpController {
+
+    @Autowired
+    private ICreativeService creativeService;
+
+    @PostMapping("/creative/create")
+    public CreativeResponse createCreative(
+            @RequestBody CreativeRequest request) throws AdException {
+        log.info("ad-sponsor: creative-create -> {}", JSON.toJSONString(request));
+        return creativeService.createCreative(request);
+    }
+}
