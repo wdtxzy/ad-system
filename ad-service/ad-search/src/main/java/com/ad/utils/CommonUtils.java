a085148af28d74a0b5ce78b616ef4222e9fc2d51
Wang Di <wangdi@xiaobangtouzi.com>
2019-06-19 23:47:46 +0800

    修改配置文件

--- ad-service/ad-search/src/main/java/com/ad/utils/CommonUtils.java
+++ ad-service/ad-search/src/main/java/com/ad/utils/CommonUtils.java
@@ -13,4 +13,14 @@ public class CommonUtils {
                                       Supplier<V> factory){
         return map.computeIfAbsent(key,k -> factory.get());
     }
+
+    public static String stringConcat(String... args){
+        StringBuilder result = new StringBuilder();
+        for (String arg: args){
+            result.append(arg);
+            result.append("-");
+        }
+        result.deleteCharAt(result.length()-1);
+        return result.toString();
+    }
 }
