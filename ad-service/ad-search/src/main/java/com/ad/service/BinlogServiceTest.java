5c49c1e249b0252c8c49eabccfe4a7429470a260
Wang Di <wangdi@xiaobangtouzi.com>
2019-06-20 23:34:59 +0800

    binlog的测试代码

---
+++ ad-service/ad-search/src/main/java/com/ad/service/BinlogServiceTest.java
@@ -0,0 +1,40 @@
+package com.ad.service;
+
+import com.github.shyiko.mysql.binlog.BinaryLogClient;
+import com.github.shyiko.mysql.binlog.event.DeleteRowsEventData;
+import com.github.shyiko.mysql.binlog.event.EventData;
+import com.github.shyiko.mysql.binlog.event.UpdateRowsEventData;
+import com.github.shyiko.mysql.binlog.event.WriteRowsEventData;
+
+/**
+ * @Author : wangdi
+ * @Date : create in 2019/6/20 23:29
+ */
+public class BinlogServiceTest {
+
+    public static void main(String[] args)throws Exception {
+
+        BinaryLogClient client = new BinaryLogClient(
+                "127.0.0.1",
+                3306,
+                "root",
+                "root"
+        );
+
+        client.registerEventListener(event -> {
+            EventData data = event.getData();
+            if(data instanceof UpdateRowsEventData){
+                System.out.println("Update------------");
+                System.out.println(data.toString());
+            }else if (data instanceof WriteRowsEventData){
+                System.out.println("Write----------");
+                System.out.println(data.toString());
+            }else if (data instanceof DeleteRowsEventData){
+                System.out.println("Delete------------");
+                System.out.println(data.toString());
+            }
+        });
+
+        client.connect();
+    }
+}
