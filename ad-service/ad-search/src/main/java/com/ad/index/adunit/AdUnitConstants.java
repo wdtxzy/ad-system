0ac0fafc1068d9bec5d6f423bbe4a83431b655f0
Wang Di <wangdi@xiaobangtouzi.com>
2019-07-03 23:09:17 +0800

    广告类型的常量定义

---
+++ ad-service/ad-search/src/main/java/com/ad/index/adunit/AdUnitConstants.java
@@ -0,0 +1,24 @@
+package com.ad.index.adunit;
+
+/**
+ * @Author : wangdi
+ * @Date : create in 2019/7/2 23:15
+ */
+public class AdUnitConstants {
+
+    /**
+     * 广告位类型
+     */
+    public static class POSITION_TYPE{
+
+        public static final Integer KAIPING = 1;
+
+        public static final Integer TIEPIAN = 2;
+
+        public static final Integer TIEPIAN_MIDDLE = 4;
+
+        public static final Integer TIEPIAN_PAUSE = 8;
+
+        public static final Integer TIEPIAN_POST = 16;
+    }
+}
